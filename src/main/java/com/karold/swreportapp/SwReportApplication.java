bd708eecf7bd916fc147bcf00ed855bf4c3b0e4c
KarolDlu <dlugolecki.karol5@gmail.com>
2022-02-24 00:20:18 +0100

    Initial commit

---
+++ src/main/java/com/karold/swreportapp/SwReportApplication.java
@@ -0,0 +1,15 @@
+package com.karold.swreportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class SwReportApplication {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SpringApplication.run(SwReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karold/swreportapp/SwReportApplicationTests.java
@@ -0,0 +1,15 @@
+package com.karold.swreportapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest
+class SwReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
